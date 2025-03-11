439c838cc1056c3eead1fc23ee9b234c8c470a09
Peyman esfandmazd <p.esfandmazd313@gmail.com>
2025-03-11 23:53:25 +0330

    add triangle owl-dot.(it has bug-test version)

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,26 +1,21 @@
 $(document).ready(function () {
-		$('.changeLanguage li a').click(function(){
+	$('.changeLanguage li a').click(function(){
 		$(this).siblings('ul').toggleClass('show');
 	});
-	const indicators = document.querySelectorAll('.carousel-indicators li');
+});
+const indicators = document.querySelectorAll('.carousel-indicators li');
 const triangle = document.querySelector('.indicator-triangle');
-
 // وقتی که اسلاید تغییر می‌کند
 const updateTrianglePosition = () => {
     // پیدا کردن دات فعال
     const activeDot = document.querySelector('.carousel-indicators .active');
-    
+
     // موقعیت افقی دات فعال
-    const activeDotPosition = activeDot.offsetLeft;
-    
+    const activeDotPosition = activeDot.offsetLeft + 7 ;
+
     // تنظیم موقعیت مثلث
     triangle.style.transform = `translateX(${activeDotPosition}px)`;
 };
 
-// گوش دادن به تغییرات اسلاید
 document.querySelector('#carouselExampleIndicators').addEventListener('slid.bs.carousel', updateTrianglePosition);
-
-// هنگام بارگذاری اولیه، موقعیت مثلث تنظیم می‌شود
 window.addEventListener('load', updateTrianglePosition);
-
-});
