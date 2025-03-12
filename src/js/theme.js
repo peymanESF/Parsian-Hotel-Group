c0437251ef9ef885d75f8996d1d2c0b7b30f3530
p.esfandmazd <p.esfandmazd@douran.ir>
2025-03-12 11:25:20 +0330

    Complete slider.indicators bug fixed.

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-	$('.changeLanguage li a').click(function(){
-		$(this).siblings('ul').toggleClass('show');
-	});
+    $('.changeLanguage li a').click(function () {
+        $(this).siblings('ul').toggleClass('show');
+    });
 });
 const indicators = document.querySelectorAll('.carousel-indicators li');
 const triangle = document.querySelector('.indicator-triangle');
 // وقتی که اسلاید تغییر می‌کند
 const updateTrianglePosition = () => {
-    // پیدا کردن دات فعال
-    const activeDot = document.querySelector('.carousel-indicators .active');
+    setTimeout(() => {
+        // پیدا کردن دات فعال
+        const activeDot = document.querySelector('.carousel-indicators .active');
 
-    // موقعیت افقی دات فعال
-    const activeDotPosition = activeDot.offsetLeft + 7 ;
+        // موقعیت افقی دات فعال
+        const activeDotPosition = activeDot.offsetLeft;
 
-    // تنظیم موقعیت مثلث
-    triangle.style.transform = `translateX(${activeDotPosition}px)`;
+        // تنظیم موقعیت مثلث
+        triangle.style.transform = `translateX(${activeDotPosition}px)`;
+    }, 1);
 };
 
-document.querySelector('#carouselExampleIndicators').addEventListener('slid.bs.carousel', updateTrianglePosition);
+document.querySelector('#carouselExampleIndicators').addEventListener('slide.bs.carousel', updateTrianglePosition);
 window.addEventListener('load', updateTrianglePosition);
